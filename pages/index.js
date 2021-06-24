591eac5949124a417befc1b9c8d77731ec2ce83f
Chong <monkey.d.chong@gmail.com>
2021-06-24 11:00:17 +0700

    first commit

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Line from "next/link";
+
+export default function index() {
+  return (
+    <div>
+      <h1>Hello, Next tutorial 2021</h1>
+
+      <nav></nav>
+    </div>
+  );
+}
---
+++ pages/post/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Post() {
+  return (
+    <div>
+      <h1>This Post</h1>
+    </div>
+  );
+}
